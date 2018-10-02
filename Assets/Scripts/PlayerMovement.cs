c3e3204c8b9bb673e09f0eb3db59177735b5aa61
Jackson Cella <cjc771@nyu.edu>
2018-10-02 05:16:05 -0400

    Initial Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+ 
+ 	public CharacterController cc;
+ 
+ 	private float speed = 30f;
+ 	void Update () {
+ 		Vector3 moveDirectionForward = transform.forward;
+ 		if (Input.GetKey(KeyCode.W))
+ 		{
+ 		
+ 			cc.Move(moveDirectionForward*speed*Time.deltaTime);
+ 		}
+ 
+ 		if (Input.GetKey(KeyCode.A))
+ 		{
+ 			transform.eulerAngles += new Vector3(0f,-3f,0f);
+ 		}
+ 		if (Input.GetKey(KeyCode.D))
+ 		{
+ 			transform.eulerAngles += new Vector3(0f,3f,0f);
+ 		}
+ 	}
+ }
